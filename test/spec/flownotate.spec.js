ff056b1b12f14020caee742ca492f358bfaad85c
Jarno Rantanen <jarno@jrw.fi>
2014-11-29 19:22:38 +0200

    Removed the intermediary directories from fixtures, they were annoying.
    Also switched all expected-output file extensions to "ts".

--- test/spec/flownotate.spec.js
+++ test/spec/flownotate.spec.js
@@ -11,29 +11,29 @@ describe('flownotate', function() {
     describe('jsToJsx()', function() {
 
         it('transforms a function\'s return type', function() {
-            var input = getFixture('function-return-type-only/in.js');
-            var expectedOutput = getFixture('function-return-type-only/out.jsx');
+            var input = getFixture('function-return-type-only.js');
+            var expectedOutput = getFixture('function-return-type-only.ts');
             var actualOutput = flownotate.jsToJsx(input);
             actualOutput.should.equal(expectedOutput);
         });
 
         it('transforms a function\'s argument types', function() {
-            var input = getFixture('function-argument-types-only/in.js');
-            var expectedOutput = getFixture('function-argument-types-only/out.jsx');
+            var input = getFixture('function-argument-types-only.js');
+            var expectedOutput = getFixture('function-argument-types-only.ts');
             var actualOutput = flownotate.jsToJsx(input);
             actualOutput.should.equal(expectedOutput);
         });
 
         it('transforms a function\'s argument AND return types', function() {
-            var input = getFixture('function-argument-and-return-types/in.js');
-            var expectedOutput = getFixture('function-argument-and-return-types/out.jsx');
+            var input = getFixture('function-argument-and-return-types.js');
+            var expectedOutput = getFixture('function-argument-and-return-types.ts');
             var actualOutput = flownotate.jsToJsx(input);
             actualOutput.should.equal(expectedOutput);
         });
 
         it('leaves already-transformed source untouched', function() {
-            var input = getFixture('transform-stability/in.js');
-            var expectedOutput = getFixture('transform-stability/out.jsx');
+            var input = getFixture('transform-stability.js');
+            var expectedOutput = getFixture('transform-stability.ts');
             var actualOutput = flownotate.jsToJsx(input);
             actualOutput.should.equal(expectedOutput);
         });
