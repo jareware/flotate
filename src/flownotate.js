1d26977448c1aac1e2942362dce0ecb02339df30
Jarno Rantanen <jarno@jrw.fi>
2014-11-29 15:35:03 +0200

    Added recursive copy of the workspace to a temp-location, with conditional in-place transformation of eligible flownotate'd files.

--- src/flownotate.js
+++ src/flownotate.js
@@ -1,5 +1,15 @@
+var path = require('path');
+var fs = require('fs');
 var esprima = require('esprima-fb');
 var falafel = require('falafel');
+var temp = require('temp');
+var wrench = require('wrench');
+
+var TEMP_DIR_NAME = 'flownotate';
+var EXCLUDED_PATHS = /(\.git|node_modules)/;
+var ELIGIBLE_FILE_EXTS = [ '.js', '.jsx' ];
+var TRIGGER_PATTERN = /^\/\* *@flow/;
+var ASSUMED_ENCODING = 'utf8';
 
 function debug(object) {
     console.log(JSON.stringify(object, undefined, 4));
@@ -33,3 +43,26 @@ function jsToJsx(jsSource) {
 }
 
 exports.jsToJsx = jsToJsx;
+
+function transformFileInPlace(filePath) {
+    if (ELIGIBLE_FILE_EXTS.indexOf(path.extname(filePath)) === -1) {
+        return; // uninteresting file type
+    }
+    var fileContent = fs.readFileSync(filePath, ASSUMED_ENCODING);
+    if (!TRIGGER_PATTERN.test(fileContent)) {
+        return; // non-flow-annotated file // TODO: What about $ flow check --all though..?
+    }
+    console.log('Transformed: ' + filePath);
+    fs.writeFileSync(filePath, jsToJsx(fileContent), { encoding: ASSUMED_ENCODING });
+}
+
+function flowCheck(sourcePath) {
+    temp.track(); // automatically track and cleanup files at exit
+    var tempDir = path.join(temp.mkdirSync(TEMP_DIR_NAME), TEMP_DIR_NAME);
+    wrench.copyDirSyncRecursive(sourcePath, tempDir, { exclude: EXCLUDED_PATHS });
+    process.chdir(tempDir);
+    console.log('Operating in: ' + tempDir);
+    wrench.readdirSyncRecursive('.').forEach(transformFileInPlace);
+}
+
+exports.flowCheck = flowCheck;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-var fs = require('fs');
 var flownotate = require('./flownotate');
 
-var file = fs.readFileSync('test/fixtures/simple-function/in.js', 'utf8');
-
-console.log(flownotate.jsToJsx(file));
+flownotate.flowCheck('.');
