30c45ba4267b19c4389630fe7e7470f43b5b56d8
Jarno Rantanen <jarno@jrw.fi>
2015-01-01 14:51:13 +0200

    Made the CLI slightly more helpful on unexpected input.

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-require(__dirname + '/flotate').flowCheck(process.argv[2] || '.');
+try {
+    if (!process.argv[2]) {
+        console.log('Usage: flotate [COMMAND]');
+    } else if (process.argv[2] === 'check') {
+        require(__dirname + '/flotate').flowCheck(process.argv[3] || '.');
+    } else {
+       throw new Error('Only the "check" command is currently supported; try "flotate check ."');
+    }
+} catch (e) {
+    console.log(e + ''); // print the error
+}
